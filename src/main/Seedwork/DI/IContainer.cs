d9c13dbc45c9da69509dc599cc16c4f3a96b8cb7
Vamsee M Kamabathula <vamsee.mohan@gmail.com>
2015-04-07 11:41:13 +0530

    added comments

--- src/main/Seedwork/DI/IContainer.cs
+++ src/main/Seedwork/DI/IContainer.cs
@@ -5,6 +5,15 @@ namespace Common.Infrastructure.Seedwork.DI
 {
     public interface IContainer : IDisposable
     {
+        /// <summary>
+        /// Build an object of the specified <paramref name="t">type</paramref>.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns>
+        /// An object of type <paramref name="t"/> or <c>null</c> if <paramref name="t"/> is 
+        /// no registered type is assignable to <paramref name="t"/>.
+        /// </returns>
+        /// <seealso cref="Build{t}"/>
         object Build(Type t);
 
         T Build<T>();
@@ -15,8 +24,19 @@ namespace Common.Infrastructure.Seedwork.DI
 
         IContainer BuildChildContainer();
 
+        /// <summary>
+        /// Wires the dependencies on the <paramref name="instance"/> using property injection.
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <returns></returns>
         object BuildObject(object instance);
 
+        /// <summary>
+        /// Returns <c>true</c> if the specified <paramref name="t">type</paramref> is registered with
+        /// the container.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         bool HasType(Type t);
     }
 }
