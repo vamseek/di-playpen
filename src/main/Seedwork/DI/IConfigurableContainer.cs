af6622a7e67709850f07e539f66555d0b3b63f8e
Vamsee M Kamabathula <vamsee.mohan@gmail.com>
2015-04-07 11:44:59 +0530

    defined configurable container functionality

--- src/main/Seedwork/DI/IConfigurableContainer.cs
+++ src/main/Seedwork/DI/IConfigurableContainer.cs
@@ -1,9 +1,19 @@
-﻿namespace Common.Infrastructure.Seedwork.DI
+﻿using System;
+
+namespace Common.Infrastructure.Seedwork.DI
 {
     public interface IConfigurableContainer : IContainer
     {
-        void RegisterSingleton(object obj);
+        void RegisterSingleton(Type lookupType, object instance);
 
+        void RegisterType(Type objectType, bool singleton, bool unitOfWorkScope);
 
+        void RegisterType<T>(Func<IContainer, T> factoryMethod, bool singleton, bool unitOfWorkScope);
+
+        void ConfigureType(Type objectType, string property, object value);
+
+        void ConfigureType<TObjectType, TSourceType>(
+            string propertyName,
+            Func<TSourceType, object> valueExpression);
     }
 }
